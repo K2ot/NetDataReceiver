0b54a2a7d678775f443747aff1df2aabc2a9d632
Krzysztof Kot <krzysztof.e.kot@gmail.com>
2024-08-02 21:46:58 +0200

    testy

--- NetDataReceiver/DataHandler.cpp
+++ NetDataReceiver/DataHandler.cpp
@@ -1,5 +1,5 @@
 #include "DataHandler.h"
-#include <mutex>
+
 
 DataManager::DataManager()
 {
@@ -27,24 +27,22 @@ void DataManager::InitializeHandlers()
 	AddHandler(handlers, message);
 }
 
-void DataManager::PushReceivedDataToTCPHandler(const std::vector<unsigned char>& frame)
+void DataManager::PushDataToTCPHandler(const std::vector<unsigned char>& frame)
 {
 	uint8_t codeHead{ 0xFF };
-	codeHead = getCodeHead(static_cast<uint8_t>(GlobalData::FrameCore::Header), frame);
-
-
-	erResCode = static_cast<bool>(codeRes >> 4);
+	codeHead = frame.at(static_cast<uint8_t>(FrameLayout::Head));
+		
 
 	if (!IsCheckSumValid(frame))
 	{
 
-		throw std::invalid_argument("Niewlasciwa sumakontrolna" + std::to_string(codeHead) + ' ' + std::to_string(codeRes));
+		throw std::invalid_argument("Niewlasciwa sumakontrolna" );
 	}
 
 	// w zaleznosci jaki jest kod rozkazu do takiego handlara wyslij same dane
 	// do handlerow maja byc juz gole dane bez otoczek ramki
 
-	std::vector<unsigned char> data{ GetData(static_cast<uint8_t>(dataSize), frame) };
+	std::vector<unsigned char> data{ GetData( frame) };
 
 	try
 	{
@@ -56,14 +54,14 @@ void DataManager::PushReceivedDataToTCPHandler(const std::vector<unsigned char>&
 		{
 		case static_cast<uint8_t>(CodeTcp::Image):
 		
-			image.Handle(erResCode, data);
+			image.Handle(data);
 			break;
 		
 		case static_cast<uint8_t>(CodeTcp::Message):
 
 			for (auto& handler : handlers)
 			{
-				handler.get().Handle(erResCode, data);
+				handler.get().Handle(data);
 				break;
 			}
 			break;
@@ -74,26 +72,26 @@ void DataManager::PushReceivedDataToTCPHandler(const std::vector<unsigned char>&
 	}
 	catch (const std::length_error& e)
 	{
-		std::count << "Pozor" << e.what();
+		std::cout << "Pozor" << e.what();
 		throw;
 	}
 	catch (const std::runtime_error& e)
 	{
-		tsc_MessageBox::Show("Pozor", e.what());
+		std::cout<<"Pozor"<< e.what();
 		throw;
 	}
 }
 
 void DataManager::popFromQueue()
 {
-	const size_t CHECKSUM{ 2 };
+	const size_t CHECK_SUM_SIZE{ 2 };
 	std::vector<unsigned char> frame{};
 	int expectedDataSize{ 0 };
 	while (!stopFlag)
 	{
 		{
 			std::unique_lock<std::mutex> lock(mutex);
-			conditionVariable.wait(lock, [this]() { return inComeData; });
+			conditionVariable.wait(lock, [this]() { return incomingData; });
 
 			if (dataQueue.empty())
 			{
@@ -107,8 +105,8 @@ void DataManager::popFromQueue()
 					dataQueue.pop();
 				}
 			}
-			expectedDataSize = GetLittleEndian(frame) + CHECKSUM;
-			int receivedDataSize = dataQueue.size();
+			expectedDataSize = GetSize(frame) + CHECK_SUM_SIZE;
+			auto receivedDataSize{ dataQueue.size() };
 			if (receivedDataSize < expectedDataSize)
 			{
 				continue;
@@ -123,16 +121,64 @@ void DataManager::popFromQueue()
 				dataQueue.pop();
 			}
 
-			PushReceivedDataToTCPHandler(frame);
+			PushDataToTCPHandler(frame);
 
 			expectedDataSize = 0;
 			frame.clear();
 			if (dataQueue.empty())
 			{
-				inComeData = false;
+				incomingData = false;
 			}
 		}
 	}
+}
+
+bool DataManager::IsCheckSumValid(const std::vector<unsigned char>& frame)
+{
+	if (frame.empty())
+	{
+		throw std::length_error("Brak danych");
+	}
+
+	uint16_t counter = std::accumulate(frame.begin(), frame.end() - 2, 0);
+	auto BytesAsInt = [](const std::vector<unsigned char>& input)
+		{
+			uint16_t output{ 0 };
+			std::vector<unsigned char> data = input;
+
+			output = (data[1] << 8) | 0;
+			output |= data[0];
+
+			return output;
+		};
+
+	uint16_t number = BytesAsInt(std::vector<unsigned char>(frame.end() - 2, frame.end()));
+	return counter == number;
+}
+
+int DataManager::GetSize(const std::vector<unsigned char>& frame)
+{
+	//GetLittleEndian
+	if (frame.size() != 4)
+	{
+		throw std::invalid_argument("Input vector must have exactly 4 elements");
+	}
+	int sizeLayout{ static_cast<uint8_t>(FrameLayout::Size) };
+
+	uint32_t result = 0;
+	result |= static_cast<uint32_t>(frame.at(1)) << 0;
+	result |= static_cast<uint32_t>(frame.at(2)) << 8;
+	result |= static_cast<uint32_t>(frame.at(3)) << 16;
+	result |= static_cast<uint32_t>(frame.at(4)) << 24;
+
+	return result;
+}
+
+std::vector<unsigned char> DataManager::GetData(std::vector<unsigned char>& frame)
+{
+	std::vector<unsigned char> vector { frame.begin() + static_cast<uint8_t>(FrameLayout::Information)
+		,frame.end() - static_cast<uint8_t>(FrameLayout::checkSume) };
+	return vector;
 }
 
 
--- NetDataReceiver/DataHandler.h
+++ NetDataReceiver/DataHandler.h
@@ -1,11 +1,22 @@
 #pragma once
 #include <vector>
 #include <queue>
+#include <mutex>
+#include <iostream>
+#include <numeric>
 
 enum class CodeTcp
 {
 	Message = 0x0A
 	, Image = 0x0B
+};
+
+enum class FrameLayout
+{
+	Head = 0x00
+	, Size = 0x04
+	, Information = 0x05
+	, checkSume = 0x02
 };
 
 
@@ -15,23 +26,25 @@ namespace Handler
 	{
 		bool result{ false };
 		std::vector<unsigned char> response{};
+
 	public:
-		Handler(uint8_t res = 0) : resCode(res) {}
+
+		Handler()  {}
 		virtual ~Handler() = default;
 		virtual void Handle(std::vector<unsigned char> frame) = 0;
 		virtual void Reset() = 0;
 		virtual bool Result() { return result; };
 		virtual std::vector<unsigned char> GetResponse() { return response; };
-		uint8_t resCode;
 	};
 
 	class Message : public Handler
 	{
 		bool result{ false };
 		std::vector<unsigned char> response{};
+
 	public:
 
-		Message() : Handler(static_cast<uint8_t>(CodeTcp::Message)) {}
+		Message() : Handler() {}
 		void Handle(std::vector<unsigned char> frame) override;
 		void Reset() override;
 		bool Result() override;
@@ -42,9 +55,10 @@ namespace Handler
 	{
 		bool result{ false };
 		std::vector<unsigned char> response{};
+
 	public:
 
-		Image() : Handler(static_cast<uint8_t>(CodeTcp::Image)) {}
+		Image() : Handler() {}
 		void Handle(std::vector<unsigned char> frame) override;
 		void Reset() override;
 		bool Result() override;
@@ -65,7 +79,11 @@ class DataManager
 	// dodac nastepne kontenery jak bedzie kreslone wiecej opcji
 	std::vector<std::reference_wrapper<Handler::Handler>> handlers;
 
-	bool inComeData{ false };
+	bool IsCheckSumValid(const std::vector<unsigned char>& frame);
+	int GetSize(const std::vector<unsigned char>& frame);
+	std::vector<unsigned char> GetData(std::vector<unsigned char>& frame);
+
+	bool incomingData{ false };
 	bool stopFlag{ false };
 	std::thread popQueueThread;
 	std::queue<unsigned char> dataQueue;
@@ -73,8 +91,7 @@ class DataManager
 	const short _SAMPLE{ 20 };
 	const short _DELAY{ 200 };
 	DataManager();
-	bool stopFlag;
-
+	
 
 public:
 
@@ -88,9 +105,11 @@ public:
 
 
 	//przekazanie danych do handlera (TCP)
-	void PushReceivedDataToTCPHandler(const std::vector<unsigned char>& frame);
+	void PushDataToTCPHandler(const std::vector<unsigned char>& frame);
 
 	void popFromQueue();
+
+	
 
 	Handler::Image image;
 	Handler::Message message;
